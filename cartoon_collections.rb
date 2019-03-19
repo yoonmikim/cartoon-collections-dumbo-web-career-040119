40f73d524e29f719f600b107147ef6801df11e6e
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-19 22:31:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.find {|el| == cheese_types }
+  arr.find {|el| cheese_types.include?(el) }
 end
